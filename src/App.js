a0971bf059b13da871b2acfab4381e3dba5c612c
Jeel Tejani <20it151@charusat.edu.in>
2022-09-07 21:08:44 +0530

    Created Initial UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Student from './components/Student';
+import StudentList from './components/StudentList';
 
 function App() {
   return (
@@ -18,8 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Student/>
+      <StudentList/>
     </div>
   );
 }
-
 export default App;
