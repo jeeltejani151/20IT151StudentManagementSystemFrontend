dc728656d990caa6f8e1103841676e144b577bea
Jeel Tejani <20it151@charusat.edu.in>
2022-09-07 21:29:02 +0530

    Added Footer.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import Student from './components/Student';
 import StudentList from './components/StudentList';
 import NavigationBar from './components/NavigationBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,6 +9,7 @@ function App() {
       <NavigationBar/>
       <Student/>
       <StudentList/>
+      <Footer/>
     </div>
   );
 }
